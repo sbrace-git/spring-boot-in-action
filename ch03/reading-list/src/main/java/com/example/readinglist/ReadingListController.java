0a1a8d2bcce0c72fcc34aee262870cddac8dc087
sbrace <sbrace0822@gmail.com>
2023-02-11 01:00:28 +0800

    ch03 reading-list init

---
+++ ch03/reading-list/src/main/java/com/example/readinglist/ReadingListController.java
@@ -0,0 +1,39 @@
+package com.example.readinglist;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/readingList")
+public class ReadingListController {
+
+    private final ReadingListRepository readingListRepository;
+
+    public ReadingListController(ReadingListRepository readingListRepository) {
+        this.readingListRepository = readingListRepository;
+    }
+
+
+    @RequestMapping(path = "/{reader}", method = RequestMethod.GET)
+    public String readersBooks(@PathVariable("reader") String reader, Model model) {
+
+        List<Book> readingList = readingListRepository.findByReader(reader);
+        if (readingList != null) {
+            model.addAttribute("books", readingList);
+        }
+        return "readingList";
+    }
+
+    @RequestMapping(path = "/{reader}", method = RequestMethod.POST)
+    public String addToReadingList(@PathVariable("reader") String reader, Book book) {
+        book.setReader(reader);
+        readingListRepository.save(book);
+        return "redirect:/readingList/{reader}";
+    }
+
+}
