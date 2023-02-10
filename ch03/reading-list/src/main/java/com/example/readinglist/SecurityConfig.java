9a398a0b27600ee92cb8a5198db240f1d8002815
sbrace <sbrace0822@gmail.com>
2023-02-11 03:07:23 +0800

    ch03 reading-list web security config

--- ch03/reading-list/src/main/java/com/example/readinglist/SecurityConfig.java
+++ ch03/reading-list/src/main/java/com/example/readinglist/SecurityConfig.java
@@ -1,20 +1,54 @@
 package com.example.readinglist;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 public class SecurityConfig {
-
+    @Autowired
+    private ReaderRepository readerRepository;
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         System.out.println("SecurityConfig securityFilterChain");
         http
+                .authorizeRequests()
+                .anyRequest().hasRole("READER")
+
+                .and()
+                .formLogin()
+
+                .and()
+
                 .csrf().disable();
         return http.build();
     }
 
+    @Bean
+    UserDetailsService userDetailsService() {
+        System.out.println("SecurityConfig userDetailsService");
+        return username -> readerRepository.findById(username).orElseGet(this::defaultUser);
+    }
+
+    @Bean
+    Reader defaultUser() {
+        System.out.println("SecurityConfig defaultUser");
+        Reader reader = new Reader();
+        reader.setUsername("user");
+        reader.setPassword(passwordEncoder().encode("1111"));
+        reader.setFullname("test-user");
+        return reader;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        System.out.println("SecurityConfig passwordEncoder");
+        return new BCryptPasswordEncoder();
+    }
 }
