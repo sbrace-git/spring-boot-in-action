0d33781908c7744487aa83f556921ecb665883d3
sbrace <sbrace0822@gmail.com>
2023-02-11 23:04:22 +0800

    ch02 reading-list test log

---
+++ ch02/reading-list/src/main/java/com/example/readinglist/Log4j2Test.java
@@ -0,0 +1,22 @@
+package com.example.readinglist;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Log4j2Test implements CommandLineRunner {
+
+    Logger logger = LogManager.getLogger();
+
+    @Override
+    public void run(String... args) {
+        logger.trace("trace");
+        logger.debug("debug");
+        logger.info("info");
+        logger.warn("warn");
+        logger.error("error");
+        logger.fatal("fatal");
+    }
+}
