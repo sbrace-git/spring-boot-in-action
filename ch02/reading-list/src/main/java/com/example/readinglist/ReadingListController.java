b43165c4b3ae097a28161730c074647306d1a102
sbrace <sbrace0822@gmail.com>
2023-02-12 15:56:47 +0800

    Revert "ch02 reading-list @ConfigurationProperties"
    This reverts commit a99b676547d3a541a01781448d3b83b13c9adf1f.

--- ch02/reading-list/src/main/java/com/example/readinglist/ReadingListController.java
+++ ch02/reading-list/src/main/java/com/example/readinglist/ReadingListController.java
@@ -1,5 +1,6 @@
 package com.example.readinglist;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,22 +11,28 @@ import java.util.List;
 
 @Controller
 @RequestMapping("/")
+@ConfigurationProperties(prefix = "amazon")
 public class ReadingListController {
 
     private final ReadingListRepository readingListRepository;
-    private final AmazonProperties amazonProperties;
 
-    public ReadingListController(ReadingListRepository readingListRepository, AmazonProperties amazonProperties) {
-        this.readingListRepository = readingListRepository;
-        this.amazonProperties = amazonProperties;
+    private String associateId;
+
+    public void setAssociateId(String associateId) {
+        this.associateId = associateId;
     }
+
+    public ReadingListController(ReadingListRepository readingListRepository) {
+        this.readingListRepository = readingListRepository;
+    }
+
 
     @RequestMapping(path = "/{reader}", method = RequestMethod.GET)
     public String readersBooks(@PathVariable("reader") String reader, Model model) {
 
         List<Book> readingList = readingListRepository.findByReader(reader);
         if (readingList != null) {
-            model.addAttribute("amazonID", amazonProperties.getAssociateId());
+            model.addAttribute("amazonID", associateId);
             model.addAttribute("books", readingList);
         }
         return "readingList";
