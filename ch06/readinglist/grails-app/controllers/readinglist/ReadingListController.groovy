06f466773f857125ac12f636cf652411114b41b1
sbrace <sbrace0822@gmail.com>
2023-02-15 02:49:48 +0800

    ch06 readingList add view

--- ch06/readinglist/grails-app/controllers/readinglist/ReadingListController.groovy
+++ ch06/readinglist/grails-app/controllers/readinglist/ReadingListController.groovy
@@ -1,6 +1,18 @@
 package readinglist
 
+import grails.gorm.transactions.Transactional
+
+
 class ReadingListController {
 
-    def index() { }
+    def index() {
+        respond Book.list(params), model: [book: new Book()]
+    }
+
+    @Transactional
+    def save(Book book) {
+        book.reader = 'Craig'
+        book.save flush: true
+        redirect(action: "index")
+    }
 }
