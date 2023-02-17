dccd69a247ec788e5a83a73a7900c89df61f89c4
sbrace <sbrace0822@gmail.com>
2023-02-17 23:00:38 +0800

    ch08 reading-list-war fixed BookServiceTest

--- ch08/reading-list-war/src/test/java/com/example/readinglist/BookServiceTest.java
+++ ch08/reading-list-war/src/test/java/com/example/readinglist/BookServiceTest.java
@@ -1,17 +1,30 @@
 package com.example.readinglist;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 @SpringBootTest
 public class BookServiceTest {
 
-    @Autowired
+    @MockBean
     private BookService bookService;
 
     @Test
     public void testService() {
+        Book bookDb = new Book();
+        bookDb.setId(1L);
+        bookDb.setAuthor("1");
+        bookDb.setDescription("1");
+        bookDb.setIsbn("1");
+        bookDb.setReader("test");
+        bookDb.setTitle("1");
+
+
+        Mockito.when(bookService.findById(1L)).thenReturn(bookDb);
         Book book = bookService.findById(1L);
 
         System.out.println(book.getId());
